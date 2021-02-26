ee54b5a5e4be10328375b432b9a4fec69a6bd17d
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2021-02-27 00:37:57 +0100

    docs: add a README.md

--- husky.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'commit-msg': 'commitlint -e $HUSKY_GIT_PARAMS',
-    'pre-commit': 'npm run-script lint-staged',
-    'pre-push': 'npm run-script test:ci',
-  },
-};
