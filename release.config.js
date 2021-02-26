fba9a614158732b0252208dec5d1660a171a9384
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2021-02-27 00:59:47 +0100

    ci: remove semantic-release changelog

--- release.config.js
+++ release.config.js
@@ -3,7 +3,6 @@ module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     '@semantic-release/github',
   ],
 };
